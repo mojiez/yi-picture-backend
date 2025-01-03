6171f4a1555db7e8bad6f4a6f7bbe8d37a507ce8
mojiez <937691466@qq.com>
2025-01-03 22:47:39 +0800

    first commit 配置了本地数据库

---
+++ src/main/java/com/atyichen/yipicturebackend/YiPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atyichen.yipicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiPictureBackendApplication.class, args);
+    }
+
+}
